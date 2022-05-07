2c3674b00db283da25b153adf1c6ce956cabef98
Lucien <Lucien@qq.com>
2022-05-07 18:31:02 +0800

    4

--- src/main/java/com/gitT.java
+++ src/main/java/com/gitT.java
@@ -8,6 +8,7 @@ package com;
 public class gitT {
  public static void main(String[] args) {
   System.out.println("git");
-  System.out.println("git4");
+  System.out.println("git4");  System.out.println("git5");
+
  }
 }
